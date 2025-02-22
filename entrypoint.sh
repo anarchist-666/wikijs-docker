b17b2fc3b05ac0bd7391124701f0899f4ef2daad
Анархист <103387254+anarchist-666@users.noreply.github.com>
2025-02-22 23:12:18 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-echo "Starting Wiki.js..."
-node server
+cd /home/container
+
+# Выводим текущую версию Node.js
+node -v
+
+# Подставляем переменные окружения в команду запуска
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Запускаем сервер
+${MODIFIED_STARTUP}
